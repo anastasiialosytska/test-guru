ca97e8b7eb8faf5004a3120eeb230e855a9936a0
Anastasiia.Losytska <alosytska@gmail.com>
2021-12-24 16:58:05 +0200

    add tests_by_category method to tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(category_title)
+    test_category_id = Category.find_by(title: category_title).id
+    Test.where(category_id: test_category_id).order('title DESC').pluck(:title)
+  end
 end
