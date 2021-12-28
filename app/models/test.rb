237377e14d3e8051dd39f03748e14c0b5bf9b2ff
Anastasiia.Losytska <alosytska@gmail.com>
2021-12-28 18:01:38 +0200

    fix relations

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_and_belongs_to_many :users
-  belongs_to :author, class: 'User', foreign_key: author_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   def self.tests_by_category(category_title)
     Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: {title: category_title}).
