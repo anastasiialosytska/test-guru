319a53c7ad42fe3caa36ac477ddb02ca5aeb988d
Anastasiia.Losytska <alosytska@gmail.com>
2021-12-28 20:13:42 +0200

    fix relations

--- db/migrate/20211228152217_add_author_id_to_tests.rb
+++ db/migrate/20211228152217_add_author_id_to_tests.rb
@@ -1,5 +1,5 @@
 class AddAuthorIdToTests < ActiveRecord::Migration[6.1]
   def change
-    add_column :tests, :author_id, :integer, foreign_key: true
+    add_reference :tests, :author, foreign_key: { to_table: :users }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_12_28_152217) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -63,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_12_28_152217) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
   add_foreign_key "tests_users", "tests"
   add_foreign_key "tests_users", "users"
 end
