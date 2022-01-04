4e90412495f242e8bdb7029c2989ea1e92f28ba4
Anastasiia.Losytska <alosytska@gmail.com>
2022-01-04 14:45:20 +0200

    add scopes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  scope :only_correct, -> { where(correct: true) }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -3,11 +3,12 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :questions
   has_and_belongs_to_many :users
-  
-  def self.tests_by_category(category_title)
-    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: {title: category_title}).
-    order('tests.title DESC').pluck(:title)
-  end
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :difficult, -> { where('level >=5 ') }
+  scope :tests_by_category, -> (category_title) { joins("INNER JOIN categories ON tests.category_id = categories.id").
+  where(categories: {title: category_title}).order('tests.title DESC').pluck(:title) }
 end
 
 
