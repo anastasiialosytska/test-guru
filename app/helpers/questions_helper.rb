07041e7113032fd051dce50a99fffc413db146b6
Anastasiia.Losytska <alosytska@gmail.com>
2022-01-14 17:13:21 +0200

    add footer, question helpers and views

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(action)
+    if action == 'new'
+      "Create New #{@question.test.title} Question"
+    elsif action == 'edit'
+      "Edit #{@question.test.title} Question"
+    end
+  end
 end
