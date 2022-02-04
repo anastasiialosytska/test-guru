c6c30815a56af0ab684c26e0739b2482bc450698
Anastasiia.Losytska <alosytska@gmail.com>
2022-02-04 14:17:52 +0200

    add Devise authentication

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+   def create
+    self.resource = warden.authenticate!(auth_options)
+    if current_user.first_name.blank?
+      flash[:notice] = 'Welcome, Guru!'
+    else
+      flash[:notice] = "Welcome, #{current_user.first_name}!"
+    end
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,9 @@ Rails.application.routes.draw do
 
   root 'tests#index'
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  delete :logout, to: 'sessions#destroy'
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }, controllers: {sessions: 'users/sessions'}
 
-  resources :users, only: :create
-  resources :sessions, only: :create
-
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true do
       resources :answers, shallow: true
     end
@@ -22,6 +17,14 @@ Rails.application.routes.draw do
   resources :test_passages, only: %i[show update] do
     member do
       get :result
+    end
+  end
+
+  namespace :admin do
+    resources :tests do
+      resources :questions, shallow: true do
+        resources :answers, shallow: true
+      end
     end
   end
 end
