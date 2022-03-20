512ec0fabf41be5d64f9dccf9e11d730638e8152
Anastasiia.Losytska <alosytska@gmail.com>
2022-03-20 17:09:16 +0200

    fixed password check

--- app/javascript/packs/utilities/check_password.js
+++ app/javascript/packs/utilities/check_password.js
@@ -8,15 +8,21 @@ function checkPassword() {
   var password = document.getElementById('password').value
   var passwordConfirmation = document.getElementById('password_confirmation').value
 
-  if (password !== passwordConfirmation) {
-    document.getElementById('message').style.color = 'red'
-    document.getElementById('message').innerHTML = 'not matching'
-    document.querySelector('.octicon-alert').classList.remove('hide')
-    document.querySelector('.octicon-check-circle').classList.add('hide')
+  if (passwordConfirmation) {
+    if (password !== passwordConfirmation) {
+      document.getElementById('message').style.color = 'red'
+      document.getElementById('message').innerHTML = 'not matching'
+      document.querySelector('.octicon-alert').classList.remove('hide')
+      document.querySelector('.octicon-check-circle').classList.add('hide')
+    } else {
+      document.getElementById('message').style.color = 'green'
+      document.getElementById('message').innerHTML = 'matching'
+      document.querySelector('.octicon-alert').classList.add('hide')
+      document.querySelector('.octicon-check-circle').classList.remove('hide')
+    }
   } else {
-    document.getElementById('message').style.color = 'green'
-    document.getElementById('message').innerHTML = 'matching'
+    document.getElementById('message').innerHTML = ''
     document.querySelector('.octicon-alert').classList.add('hide')
-    document.querySelector('.octicon-check-circle').classList.remove('hide')
+    document.querySelector('.octicon-check-circle').classList.add('hide')
   }
 }
