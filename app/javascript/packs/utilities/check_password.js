953ce0bf184de5c26c748bc84feff93e38d77d35
Anastasiia.Losytska <alosytska@gmail.com>
2022-03-21 11:42:01 +0200

    fixed password check

--- app/javascript/packs/utilities/check_password.js
+++ app/javascript/packs/utilities/check_password.js
@@ -1,28 +1,34 @@
 document.addEventListener('turbolinks:load', function() {
   var control = document.querySelector('.check-password')
 
-  if (control) { control.addEventListener('input', checkPassword) }
+  if (control) { control.addEventListener('input', checkPasswordConfirmation) }
 })
 
-function checkPassword() {
-  var password = document.getElementById('password').value
+function checkPasswordConfirmation() {
   var passwordConfirmation = document.getElementById('password_confirmation').value
-
+  
   if (passwordConfirmation) {
-    if (password !== passwordConfirmation) {
-      document.getElementById('message').style.color = 'red'
-      document.getElementById('message').innerHTML = 'not matching'
-      document.querySelector('.octicon-alert').classList.remove('hide')
-      document.querySelector('.octicon-check-circle').classList.add('hide')
-    } else {
-      document.getElementById('message').style.color = 'green'
-      document.getElementById('message').innerHTML = 'matching'
-      document.querySelector('.octicon-alert').classList.add('hide')
-      document.querySelector('.octicon-check-circle').classList.remove('hide')
-    }
+    comparePasswords()
   } else {
     document.getElementById('message').innerHTML = ''
     document.querySelector('.octicon-alert').classList.add('hide')
     document.querySelector('.octicon-check-circle').classList.add('hide')
+  }
+}
+
+function comparePasswords() {
+  var password = document.getElementById('password').value
+  var passwordConfirmation = document.getElementById('password_confirmation').value
+
+  if (password !== passwordConfirmation) {
+    document.getElementById('message').style.color = 'red'
+    document.getElementById('message').innerHTML = 'not matching'
+    document.querySelector('.octicon-alert').classList.remove('hide')
+    document.querySelector('.octicon-check-circle').classList.add('hide')
+  } else {
+    document.getElementById('message').style.color = 'green'
+    document.getElementById('message').innerHTML = 'matching'
+    document.querySelector('.octicon-alert').classList.add('hide')
+    document.querySelector('.octicon-check-circle').classList.remove('hide')
   }
 }
