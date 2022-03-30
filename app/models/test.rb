302d4e436b5ce5d064e65843c9d2128fa893fe76
Anastasiia.Losytska <alosytska@gmail.com>
2022-03-30 13:12:10 +0300

    fixed bugs

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,12 +2,13 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :questions, dependent: :destroy
-  has_many :test_passages, dependent: :delete_all
+  has_many :test_passages, dependent: :destroy
   has_many :users, through: :test_passages
 
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :difficult, -> { where('level >=5 ') }
+  scope :ready, -> { where(is_ready: true) }
   scope :tests_by_category, -> (category_title) { joins("INNER JOIN categories ON tests.category_id = categories.id").
     where(categories: { title: category_title}) }
 
